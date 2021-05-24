83f78655ee765908de8d874d720146894effd211
Rambod Rahmani <rambodrahmani@autistici.org>
2021-05-24 18:08:05 +0200

    Implement new version of the script to fetch stock data.
    This new implementation uses the IEX Cloud data provider and the pyEX
    package. Yahoo! Finance was not good enough to provide real time data in
    the long run.

--- vallati/python/fetch_stock_prices.py
+++ vallati/python/fetch_stock_prices.py
@@ -4,62 +4,54 @@
 # Retrieves last minute prices for the stock tickers defined in tikers[]. The
 # retrieved prices are pushed to the corresponding stock metric in Gnocchi DB.
 ##
+import pyEX
 import json
-import datetime
-import requests
-import functools
-import yfinance as yf
 import urllib.request
-import dateutil.parser
-from pytz import timezone
+from datetime import datetime
 
 __author__ = "Leonardo Turchetti, Lorenzo Tonelli, Ludovica Cocchella and Rambod Rahmani"
 __copyright__ = "Copyright (C) 2021 Leonardo Turchetti, Lorenzo Tunelli, Ludovica Cocchella and Rambod Rahmani"
 __license__ = "GPLv3"
 
+# pyEX engine
+c = pyEX.Client("Tpk_165f42bb152b43df82921c1d713bad5d", version='sandbox')
+
 # keystone token
-keystone_token = "gAAAAABgo3ljkfH41aeXCgwj1Ois7rLwtji13hI3MbAXpJ1sU0NJsYyVAntZWsTaMngwbVOCp4b5Q3YJJQucSUhks5aAaBHOvGVuSvEI1HB85609mynhGeEiOgIwHd7PrMQfj6v8sH5Ej7RxIx4tpgcIhNzD_yN1XlKoplbbKo5Dae4M2TmydEM"
+keystone_token = "gAAAAABgqo4EBJerAKtAUGHMUyE93A7Vnoy3XUXZ7lLOGLXki_qXc0rCX6JqFXwFbbv_VrsMud-VEjyGu5KLQDp5NEZpHIExnVhdx6BIVkRAPGdIqKyRV5KJ4ycOLiuwjVkZGurhmvHPCfL1WO6veSc0Gk3D0AsuYPX-dAwGEQbPCN23o5dC9nk"
 
 # stock tickers to be retrieved
-tickers = ["ENEL.MI", "ISP.MI", "STLA", "ENI.MI", "RACE.MI", "G.MI", "STM.MI", \
-        "UCG.MI", "CNHI.MI", "EXO.MI", "SRG.MI", "PST.MI", "MONC.MI", "ATL.MI",\
-        "TRN.MI"];
-tickers_metrics = ["faafee03-ec51-4929-b530-0452eef75464", \
-        "4042d720-7226-466c-b4aa-21e0a8ed2b96", \
-        "88cf571d-8573-4da7-8dc5-71ae369d0c0b", \
-        "bac224a0-4bb9-46ec-877b-9a0c1e2cf395", \
-        "47d0b85c-d4d3-45c1-a58f-93fd8b6adfb6", \
-        "40475954-1aa1-4c7c-889d-57cd7461ca50", \
-        "c4aa045a-c02b-4dca-9969-0c95757a46ca", \
-        "1af4592a-fc9c-4e2d-9789-d3e0029d0564", \
-        "88f6ac70-e899-409c-9f0d-f0bbcbac9b29", \
-        "3aad068e-aad2-4e51-8d63-35cb3499678c", \
-        "9eb816af-144c-4a1b-a5d6-ee22092fee8a", \
-        "86b51e5f-dca8-46ce-bff8-ccd7e74f7f8e", \
-        "473e2213-234c-4865-b71b-7bd68ed7bbf8", \
-        "014e1074-8277-4da7-acd2-57cfabc2277e", \
-        "55a269c2-3119-4a2c-b57f-9cbbc00fddd2"]
+tickers = ["AMZN", "GOOG", "BKNG", "CHTR", "NVDA", "TSLA", "NFLX", "ADBE", \
+        "FB", "MSFT", "PYPL", "ADSK", "AMD", "EBAY", "FOX"];
+tickers_metrics = ["a34a5ffb-a616-4583-99df-73885cbac719", \
+                "06a298cc-aa96-47b0-ac3a-4242536dda0e", \
+                "64499075-fcc6-4a12-bf8f-8fc6e7b5d20d", \
+                "606f0e27-7a9c-446b-8d8e-32283eeb3524", \
+                "955bf952-0f22-4a33-828b-e3046bcceb51", \
+                "ed8da2ec-fc58-48af-92f8-b746cef30a2d", \
+                "8f7cd857-b88a-4eb8-9ad7-ad361dea71ce", \
+                "e2bf842c-f010-44a5-8b91-510c524f8eee", \
+                "b634fe45-2fec-47b5-8816-c0aa672e90af", \
+                "e7a19eaa-1590-4d1d-aa98-589a3a5862f8", \
+                "d4007b77-197a-4b17-b2f2-54ee97331ae0", \
+                "1b089d84-b79b-4309-868e-0d22019e7d63", \
+                "266d9a2f-4749-4a6b-9a7c-57315c1d331d", \
+                "233cce21-8ad9-4b57-a02d-1d6a581a3fbf", \
+                "1f1f7244-bdda-456a-ad6f-7464c2c8645c"]
 
 while True:
     for i in range(len(tickers)):
-        # retrieve last minute price
-        ticker = yf.Ticker(tickers[i])
-        print("Processing: " + ticker.info['shortName'] + " [" + ticker.info["symbol"] + "]")
-        pricesDF = ticker.history(period="1d", interval="1m")
+        # retrieve current UTC timestamp
+        timestamp = datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S")
 
-        # yfinance api timeouts might result in empty dataframes
-        if not pricesDF.empty:
-            # extract new highest price and timestamp
-            priceDateString = str(pricesDF.index[-1])
-            priceValue = pricesDF['High'][-1]
-            priceDate = dateutil.parser.isoparse(priceDateString)
-            priceDate = priceDate.astimezone(timezone('UTC'))
-            print(priceDate.strftime("%Y-%m-%dT%H:%M:%S %Z") + " - " + str(priceValue))
+        # retrieve last second stock price
+        price = c.price(tickers[i])
+        print(timestamp + " | " + tickers[i] + " - " + str(price))
 
+        # check if the retrieved price is valid
+        if (price > 0.0):
             # push data to gnocchi
             conditionsSetURL = "http://252.3.238.176:8041/v1/metric/" + tickers_metrics[i] + "/measures"
-            newConditions = [{"timestamp": priceDate.strftime("%Y-%m-%dT%H:%M:%S"), "value": priceValue}]
+            newConditions = [{"timestamp": timestamp, "value": str(price)}]
             params = json.dumps(newConditions).encode('utf8')
             req = urllib.request.Request(conditionsSetURL, data=params, headers={'content-type': 'application/json', 'X-AUTH-TOKEN': keystone_token})
             response = urllib.request.urlopen(req)
-            print(response.read().decode('utf8'))
